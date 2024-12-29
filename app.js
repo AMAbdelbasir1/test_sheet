4f266e441ae80cba1f63af37bf3724172cd90f5b
AMAbdelbasir1 <addtooo140@gmail.com>
2024-12-29 16:06:10 +0200

    first commit

--- app.js
+++ app.js
@@ -9,25 +9,7 @@ require("dotenv").config();
 const app = express();
 
 // CORS configuration
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        "http://localhost:3001",
-        "https://cloud-left-task-3g4mqcjub-hassanmostfas-projects.vercel.app",
-        "https://cloud-left-task.vercel.app",
-      ];
-
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["POST", "GET"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
+app.use(cors());
 
 // Increase payload size limit
 app.use(express.json({ limit: "50mb" }));
